33e028e9d250fcd2425bb60c52de1655303b69b5
Pavel Tchoursinov <wire-less@mail.ru>
2025-11-16 22:25:12 +0300

    Classes homework release 1.01

---
+++ module02Lesson10Classes/otus-physics-master/Ball.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "Painter.hpp"
+#include "Point.hpp"
+#include "Velocity.hpp"
+
+class Ball {
+  public:
+    void setVelocity(const Velocity& velocity);
+    Velocity getVelocity() const;
+    void draw(Painter& painter) const;
+    void setCenter(const Point& center);
+    Point getCenter() const;
+    double getRadius() const;
+    double getMass() const;
+ 
+    Point ballCenter;
+    Velocity ballVelocity;
+    double ballRadius;
+    Color ballcolor;    
+};
