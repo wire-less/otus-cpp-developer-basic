ed515bdebbd77027919419b6e82cbbfbdc3d7717
Pavel Tchoursinov <wire-less@mail.ru>
2026-03-09 19:13:28 +0300

    v1.4 избавиться от std::mutex, отдельные словари для потоков

--- module05Lesson34multiThreading2/topk_words.cpp
+++ module05Lesson34multiThreading2/topk_words.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cctype>
 #include <cstdlib>
+#include <execution>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -19,7 +20,7 @@ using Counter = std::map<std::string, std::size_t>;
 
 std::string tolower(const std::string &str);
 
-void count_words(std::istream &stream, Counter &, std::mutex &mtx);
+Counter count_words(std::istream &stream);
 
 void print_topk(std::ostream &stream, const Counter &, const size_t k);
 
@@ -34,10 +35,11 @@ int main(int argc, char *argv[])
     auto start = std::chrono::high_resolution_clock::now();
 
     // Приготовиться к распараллеливанию задачи на потоки
-    std::vector<std::future<void>> workers;
-    std::mutex mtx;
+    std::vector<std::future<Counter>> workers;
 
+    // Объявить общий словарь
     Counter freq_dict;
+
     // Создать вектор для открываемых файлов
     std::vector<std::ifstream> file_streams;
     file_streams.reserve(argc - 1);
@@ -52,14 +54,18 @@ int main(int argc, char *argv[])
             return EXIT_FAILURE;
         }
 
-        // Запустить обработку каждого файла в отдельном потоке с общим mutex и единым словарем
-        workers.emplace_back(std::async(std::launch::async, count_words, std::ref(file_streams.back()), std::ref(freq_dict), std::ref(mtx)));
+        // Запустить обработку каждого файла в отдельном потоке
+        workers.emplace_back(std::async(std::launch::async, count_words, std::ref(file_streams.back())));
     }
 
-    // Подождать завершения всех потоков выполнения программы
+    // Объединить словари всех потоков в один
     for (auto &worker : workers)
     {
-        worker.get();
+        Counter counter = worker.get();
+        for (const auto &pair : counter)
+        {
+            freq_dict[pair.first] += pair.second;
+        }
     }
 
     print_topk(std::cout, freq_dict, TOPK);
@@ -78,12 +84,17 @@ std::string tolower(const std::string &str)
     return lower_str;
 };
 
-void count_words(std::istream &stream, Counter &counter, std::mutex &mtx)
+Counter count_words(std::istream &stream)
 {
+    // Создать собственный словарь для каждого вызова функции
+    Counter thread_dictionary;
+
     std::for_each(std::istream_iterator<std::string>(stream),
                   std::istream_iterator<std::string>(),
-                  [&counter, &mtx](const std::string &s)
-                  { std::lock_guard<std::mutex> lock(mtx); ++counter[tolower(s)]; });
+                  [&thread_dictionary](const std::string &s)
+                  { ++thread_dictionary[tolower(s)]; });
+
+    return thread_dictionary;                  
 }
 
 void print_topk(std::ostream &stream, const Counter &counter, const size_t k)
@@ -99,12 +110,12 @@ void print_topk(std::ostream &stream, const Counter &counter, const size_t k)
     auto middle_iter = std::begin(words) + std::min(k, words.size());
 
     std::partial_sort(
-        std::begin(words), middle_iter, std::end(words),
+        std::execution::par_unseq, std::begin(words), middle_iter, std::end(words),
         [](const auto &lhs, const auto &rhs)
         { return lhs->second > rhs->second; });
 
     std::for_each(
-        std::begin(words), middle_iter,
+        std::execution::unseq, std::begin(words), middle_iter,
         [&stream](const Counter::const_iterator &pair)
         {
             stream << std::setw(4) << pair->second << " " << pair->first
